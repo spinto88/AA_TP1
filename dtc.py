f3b53551562e0e68e557718a6cf6e38a0173ff59
g <guipasq>
2016-09-22 16:51:25 -0300

    agrego, comentadas, las lineas para analizar parametros en arboles

--- dtc.py
+++ dtc.py
@@ -15,3 +15,18 @@ for fact in [5,10,20,30,40,120,'original']:
     res = cross_val_score(clf, X, y, cv=10)
     print 'Factors: ' + str(fact)
     print "{:.3f}".format(np.mean(res)), "{:.3f}".format(np.std(res))
+
+
+# PARA INSPECCIONAR PARAMETROS
+# Elijo mi clasificador.
+#for crit in "gini", "entropy":
+#    for spl in "best", "random":
+#        for m_dep in range(1, df.shape[1]-2, (df.shape[1]-2)/10):
+#
+## Ejecuto el clasificador entrenando con un esquema de cross validation
+## de 10 folds.
+#            # Params. Trees: http://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html#sklearn.tree.DecisionTreeClassifier
+#            clf = DecisionTreeClassifier(criterion=crit, splitter=spl, max_depth=m_dep)
+#            res = cross_val_score(clf, X, y, cv=10)
+#            print "{:.3f}".format(np.mean(res)), "{:.3f}".format(np.std(res)), "[crit: {}; spl: {}; m_dep: {}]".format(crit, spl, m_dep)
+
