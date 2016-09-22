234da6fa495ae9eb14e4d8f5d3f27c557f1dbf22
Gustavo Landfried <gustavolandfried@gmail.com>
2016-09-22 09:44:58 -0300

    Informe tex y knn

--- knn.py
+++ knn.py
@@ -3,8 +3,8 @@ import numpy as np
 import cPickle as pk
 from sklearn.neighbors import KNeighborsClassifier as KNN
 from sklearn.cross_validation import cross_val_score
-import matplotlib.pyplot as plt
-
+#import matplotlib.pyplot as plt
+import csv
 # Cargo el data Frame guardado como un archivo cPickle
 df = pk.load(file('DataFrame.pk'))
 
@@ -13,14 +13,7 @@ clf = KNN()
 # X es una matriz de documentos x features
 X = df.ix[:, 2:].values
 y = df['class']
-nn = []
-#for n in [1,5,10,20,40,80,160,320,640,1280]:
-#	clf = KNN(n_neighbors=n)
-#	scores = cross_val_score(clf, X, y, cv = 10)
-#	print  (np.mean(scores), np.std(scores))
-#	nn.append( (np.mean(scores), np.std(scores)) )
-#print nn
-#n_best = np.argmin([i[0] for i in nn])+3
+
 n_best = 1
 
 clf = KNN(n_neighbors=n_best)
@@ -37,15 +30,17 @@ U, S, Vt = linalg.svd(cov)
 # Cantidad de autovalores con los que me quedo 
 # (< dim(features))
 
+res_file = open('resultados/knn.csv', 'wb') 
+res_writer = csv.writer(res_file)
+res_writer.writerow(['n_variables_svd','mean','std'])
 for eigs in [5, 10, 20, 40]:
 
     number_of_eig = eigs
     U_reduce = U[:number_of_eig]
     S_reduce = S[:number_of_eig]
-
     # Matrix reducida
-    X_reduce = X.dot(np.transpose(U_reduce))
-  
+    X_reduce = X.dot(np.transpose(U_reduce))  
     scores = cross_val_score(clf, X_reduce, y, cv = 10)
+    res_writer.writerow([eigs, np.mean(scores), np.std(scores)] )
     print 'Eig: ', number_of_eig, ' - score: ', np.mean(scores), np.std(scores)
     
