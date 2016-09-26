848bf01fbf5362725933733a7552c54c36827872
Gustavo Landfried <gustavolandfried@gmail.com>
2016-09-26 11:50:22 -0300

    Implementación Hill climbing. Imagenes random forest y features ranking

--- features-hill_climbing_selection.py
+++ features-hill_climbing_selection.py
@@ -9,28 +9,41 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier as DTC
 from sklearn.cross_validation import cross_val_score
 import csv
+"""
+file_hill = open('resultados/features-hill.csv', 'wb') 
+write = csv.writer(file_hill)
+write.writerow(['S','mean','std'])
+file_hill.close()
+"""
 
+def write_optimo_local(S, mean, std):
+	print "Optimo local!", mean, std
+	file_hill = open('resultados/features-hill.csv', 'a') 
+	write = csv.writer(file_hill)
+	write.writerow([mean,std,S])
+	file_hill.close()
+	#return 0
 
 df = pk.load(file('DataFrame.pk'))
 
 clf = DTC()
 y = df.ix[:,'class']
-for t in range(16):
+for t in range(8):
 	#t = 1
 	print '------------ Nueva buesqueda -------------- '
-	res_file = open('resultados/features-hill_climbing'+str(t)+'.csv', 'wb') 
-	res_writer = csv.writer(res_file)
-	res_writer.writerow(['S','mean','std'])
 	size = random.choice(range(len(df.columns[2:])))
 	S = list(np.random.choice(df.columns[2:],size=size, replace=False))
 	F = list(set(df.columns[2:])-set(S))
 	
 	scores = cross_val_score(clf, df.ix[:,S], y, cv = 2)
 	old_perf =  np.mean(scores)
-	
+	old_std = np.std(scores)
+
 	new_perf=old_perf
+	new_std = old_std
 	while new_perf >= old_perf:
 		old_perf = new_perf 	
+		old_std = new_std
 		f_ranking = pd.DataFrame([],columns=['f','score'])
 		f_ranking.set_index('f')
 		s_ranking = pd.DataFrame([],columns=['s','score'])
@@ -40,18 +53,24 @@ for t in range(16):
 			P.append(f)
 			scores = cross_val_score(clf, df.ix[:,P], y, cv = 2)
 			f_ranking.loc[f,'score'] =  np.mean(scores)
+			f_ranking.loc[f,'std'] =  np.std(scores)
 		for s in S:
 			#s=S[1]
 			P = list(S)
 			P.remove(s)
 			scores = cross_val_score(clf, df.ix[:,P], y, cv = 2)
 			s_ranking.loc[s,'score'] =  np.mean(scores)
+			s_ranking.loc[s,'std'] =  np.std(scores)
 		f_ranking = f_ranking.sort(columns=['score'], ascending=False) 		
 		s_ranking = s_ranking.sort(columns=['score'], ascending=False) 		
 		if len(F)==0: new_perf_f = 0
-		else: new_perf_f = f_ranking.ix[0,'score']
+		else: 
+			new_perf_f = f_ranking.ix[0,'score']
+			new_std = f_ranking.ix[0,'std']
 		if len(S)==0: new_perf_s = 0
-		else: new_perf_s = s_ranking.ix[0,'score']	
+		else: 
+			new_perf_s = s_ranking.ix[0,'score']	
+			new_std =  s_ranking.ix[0,'std']	
 		if new_perf_f>=new_perf_s:	
 			new_perf = new_perf_f
 			aumento =True
@@ -68,6 +87,6 @@ for t in range(16):
 				s_best = s_ranking.index[0]	
 				S.remove(s_best)
 				F.append(s_best)
-			res_writer.writerow([S, np.mean(scores), np.std(scores)] )
-			print new_perf, S 
-		
+			print new_perf, len(S) 
+	
+	write_optimo_local(S, old_perf, old_std)
