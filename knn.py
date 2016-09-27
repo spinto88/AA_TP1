76e65a99afe2063c1bec4825a4034b2b4c7e5d28
Gustavo Landfried <gustavolandfried@gmail.com>
2016-09-27 10:37:42 -0300

    Algunos aportes parciales. Intro. Discusión. knn

--- knn.py
+++ knn.py
@@ -8,15 +8,13 @@ import csv
 # Cargo el data Frame guardado como un archivo cPickle
 df = pk.load(file('DataFrame.pk'))
 
-clf = KNN()
 
 # X es una matriz de documentos x features
 X = df.ix[:, 2:].values
 y = df['class']
 
-n_best = 1
 
-clf = KNN(n_neighbors=n_best)
+clf = KNN(n_neighbors=1)
 
 
 number_of_doc = X.shape[0]
@@ -30,10 +28,40 @@ U, S, Vt = linalg.svd(cov)
 # Cantidad de autovalores con los que me quedo 
 # (< dim(features))
 
-res_file = open('resultados/knn.csv', 'wb') 
+res_file = open('resultados/knn_n1.csv', 'wb') 
 res_writer = csv.writer(res_file)
 res_writer.writerow(['n_variables_svd','mean','std'])
-for eigs in [5, 10, 20, 40]:
+for eigs in [5, 10, 20, 40,80,127]:
+
+    number_of_eig = eigs
+    U_reduce = U[:number_of_eig]
+    S_reduce = S[:number_of_eig]
+    # Matrix reducida
+    X_reduce = X.dot(np.transpose(U_reduce))  
+    scores = cross_val_score(clf, X_reduce, y, cv = 10)
+    res_writer.writerow([eigs, np.mean(scores), np.std(scores)] )
+    print 'Eig: ', number_of_eig, ' - score: ', np.mean(scores), np.std(scores)
+    
+
+
+clf = KNN(n_neighbors=10)
+
+
+number_of_doc = X.shape[0]
+
+# Matriz de covarianza de features x features
+cov = np.cov(np.transpose(X))
+
+# Hago una descomposicion SVD
+U, S, Vt = linalg.svd(cov)
+
+# Cantidad de autovalores con los que me quedo 
+# (< dim(features))
+
+res_file = open('resultados/knn_n10.csv', 'wb') 
+res_writer = csv.writer(res_file)
+res_writer.writerow(['n_variables_svd','mean','std'])
+for eigs in [5, 10, 20, 40,80,127]:
 
     number_of_eig = eigs
     U_reduce = U[:number_of_eig]
